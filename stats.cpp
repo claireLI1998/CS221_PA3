d82a7b06fe9ba35194a20950955c123dda9b9592
zzxyrg <51840914+zzxyrg@users.noreply.github.com>
2019-06-16 08:39:15 +0800

    Add files via upload

---
+++ stats.cpp
@@ -0,0 +1,154 @@
+
+#include "stats.h"
+
+stats::stats(PNG & im){
+
+/* YOUR CODE HERE */
+for(int i = 0; i < im.width(); i++){
+	for(int j =0; j < im.height(); j++) {
+		if(i > 0 && j > 0) {
+			sumSat[i][j] = im.getPixel(i,j) -> s + sumSat[i-1][j] + sumSat[i][j-1] - sumSat[i-1][j-1];
+		} else if (i > 0) {
+			sumSat[i][j] = im.getPixel(i,j) -> s + sumSat[i-1][j];
+		} else if (j > 0) {
+			sumSat[i][j] = im.getPixel(i,j) -> s + sumSat[i][j-1];
+		} else {
+			sumSat[i][j] = im.getPixel(i,j) -> s;
+		}
+//		sumSat[i][j] = im.getPixel(i,j) -> s;
+	}
+}
+
+for(int i = 0; i < im.width(); i++){
+	for(int j =0; j < im.height(); j++) {
+		if(i > 0 && j > 0) {
+			sumLum[i][j] = im.getPixel(i,j) -> l + sumLum[i-1][j] + sumLum[i][j-1] - sumLum[i-1][j-1];
+		} else if (i > 0) {
+			sumLum[i][j] = im.getPixel(i,j) -> l + sumLum[i-1][j];
+		} else if (j > 0) {
+			sumLum[i][j] = im.getPixel(i,j) -> l + sumLum[i][j-1];
+		} else {
+			sumLum[i][j] = im.getPixel(i,j) -> l;
+		}
+	}
+}
+
+for(int i = 0; i < im.width(); i++){
+	for(int j =0; j < im.height(); j++) {
+		if(i > 0 && j > 0) {
+			sumHueX[i][j] = cos(im.getPixel(i,j) -> h) 
+			 + sumHueX[i-1][j] + sumHueX[i][j-1] - sumHueX[i-1][j-1];
+		} else if (i > 0) {
+			sumHueX[i][j] = cos(im.getPixel(i,j) -> h)
+			 + sumHueX[i-1][j];
+		} else if (j > 0) {
+			sumHueX[i][j] = cos(im.getPixel(i,j) -> h)
+			 + sumHueX[i][j-1];
+		} else {
+			sumHueX[i][j] = cos(im.getPixel(i,j) -> h);
+		}
+//		sumHueX[i][j] = cos(im.getPixel(i,j) -> h) * sumSat[i][j];
+	}
+}
+
+for(int i = 0; i < im.width(); i++){
+	for(int j =0; j < im.height(); j++) {
+		if(i > 0 && j > 0) {
+			sumHueY[i][j] = sin(im.getPixel(i,j) -> h )
+			 + sumHueY[i-1][j] + sumHueY[i][j-1] - sumHueY[i-1][j-1];
+		} else if (i > 0) {
+			sumHueY[i][j] = sin(im.getPixel(i,j) -> h)
+			 + sumHueY[i-1][j];
+		} else if (j > 0) {
+			sumHueY[i][j] = sin(im.getPixel(i,j) -> h)
+			 + sumHueY[i][j-1];
+		} else {
+			sumHueY[i][j] = sin(im.getPixel(i,j) -> h);
+		}
+	}
+}
+
+
+for(int i = 0; i < im.width(); i++){
+	for(int j =0; j < im.height(); j++) {
+		int temp = helper1(im.getPixel(i,j) -> h);
+		for(int k = 0; k < 36; k++) {
+		
+		if(i > 0 && j > 0) {
+			if(temp == k) {
+			
+			hist[i][j][k] = hist[i-1][j][k] + hist[i][j-1][k] - hist[i-1][j-1][k] + 1;
+		    } else {
+		    hist[i][j][k] = hist[i-1][j][k] + hist[i][j-1][k] - hist[i-1][j-1][k];	
+			}
+		} else if (i > 0) {
+			if(temp == k) {
+			hist[i][j][k] = hist[i-1][j][k] + 1;
+		    } else {
+		    hist[i][j][k] = hist[i-1][j][k];	
+			}
+		} else if (j > 0) {
+			if(temp == k) {
+			hist[i][j][k] = hist[i][j-1][k] + 1;
+		    } else {
+		    hist[i][j][k] = hist[i][j-1][k];	
+			}
+		} else {
+			if(temp == k) {
+			hist[i][j][k] = 1;
+		    } else {
+		    hist[i][j][k] = 0;	
+			}
+		}
+	}
+	}
+	}
+
+}
+
+int stats::helper1(double hue) {
+	for(int i = 0; i < 36; i++) {
+		if(i*10 <= hue && hue < (i+1)*10)
+		return i;
+	}
+}
+
+long stats::rectArea(pair<int,int> ul, pair<int,int> lr){
+
+/* YOUR CODE HERE */
+return (lr.first - ul.first + 1) * (lr.second - ul.second + 1);
+
+}
+
+HSLAPixel stats::getAvg(pair<int,int> ul, pair<int,int> lr){
+
+/* YOUR CODE HERE */
+
+}
+
+vector<int> stats::buildHist(pair<int,int> ul, pair<int,int> lr){
+
+/* YOUR CODE HERE */
+
+}
+
+// takes a distribution and returns entropy
+double stats::entropy(vector<int> & distn,int area){
+
+    double entropy = 0;
+    for (int i = 0; i < 36; i++) {
+        if (distn[i] > 0 ) 
+            entropy += ((double) distn[i]/(double) area) 
+                                    * log2((double) distn[i]/(double) area);
+    }
+    return  -1 * entropy;
+
+}
+
+double stats::entropy(pair<int,int> ul, pair<int,int> lr){
+
+    vector<int> distn =  buildHist(ul,lr);
+    int area = rectArea(ul,lr);
+    return  entropy(distn,area);
+
+}
